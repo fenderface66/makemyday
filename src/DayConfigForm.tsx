3cde5a7d91514281b38fac716359171acb9c6b96
Robbie <r.heygate@gmail.com>
2021-06-15 20:24:40 +0100

    Add simple request

---
+++ src/DayConfigForm.tsx
@@ -0,0 +1,59 @@
+// Render Prop
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+type Values = {
+  email: string;
+  password: string;
+}
+
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  console.log('data for post', data);
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: new Headers({'content-type': 'application/json'}),
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+const DayConfigForm = () => (
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={(values: Values) => {
+        const errors: any = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={async (values, { setSubmitting }) => {
+        console.log("SUBMITTING", values);
+        await postData('http://localhost:3000/make-my-day', values);
+        setSubmitting(false);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+);
+
+export default DayConfigForm;
