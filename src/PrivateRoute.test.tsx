305e18b953cfeac72ec03f38b4be126312911b84
Robbie <r.heygate@gmail.com>
2021-08-17 19:27:23 +0100

    Add tests for token validation

--- src/PrivateRoute.test.tsx
+++ src/PrivateRoute.test.tsx
@@ -4,12 +4,51 @@ import {
   Switch,
   MemoryRouter,
 } from "react-router-dom";
+import Cookies from 'js-cookie';
 import { createMemoryHistory } from 'history'
 import renderer, { act } from 'react-test-renderer';
 import PrivateRoute from "./PrivateRoute";
-import Cookies from 'js-cookie';
+
+import * as api from './api';
+
+jest.mock('./api', () => ({
+  __esModule: true,
+  default: jest.fn(() => ({
+    status: 401
+  })),
+}))
+
+jest.mock('js-cookie', () => ({
+  get: jest.fn()
+}));
+
+const renderComponentWithRouting = () => {
+  let renderedHistory, renderedLocation;
+  const memoryHistory = createMemoryHistory();
+  renderer.create(
+    <Router history={memoryHistory as any}>
+      <Switch>
+        <PrivateRoute
+          render={({ history , location }: {history: History, location: Location}) => {
+            renderedHistory = history;
+            renderedLocation = location;
+            return null;
+          }}
+        />
+      </Switch>
+    </Router>
+  );
+  return {
+    renderedHistory,
+    renderedLocation,
+  }
+}
+
+
 
 describe('<PrivateRoute />', function () {
+  let testHistory: History | undefined;
+  let testLocation: Location | undefined;
   it('should check to see if a user cookie exists', async () => {
     const getCookieSpy = jest.spyOn(Cookies, 'get');
     await act(async () => {
@@ -23,27 +62,74 @@ describe('<PrivateRoute />', function () {
     });
     expect(getCookieSpy).toHaveBeenCalledWith('user');
   });
-  it('redirects to the login page when a cookie is not present', async () => {
-    let testHistory, testLocation;
-    const history = createMemoryHistory();
-    await act(async () => {
-      renderer.create(
-        <Router history={history as any}>
-          <Switch>
-            <PrivateRoute
-              render={({ history , location }: {history: History, location: Location}) => {
-                testHistory = history;
-                testLocation = location;
-                return null;
-              }}
-            />
-          </Switch>
-        </Router>
-      );
+  describe('When the cookie is not present', () => {
+    it('redirects to the login page when a cookie is not present', async () => {
+      await act(async () => {
+        const { renderedHistory, renderedLocation } = await renderComponentWithRouting();
+        testHistory = renderedHistory;
+        testLocation = renderedLocation;
+      });
+      if (!testLocation) {
+        return
+      }
+      expect((testLocation as Location).pathname).toBe("/login");
     });
-    if (!testLocation) {
-      return
-    }
-    expect((testLocation as Location).pathname).toBe("/login");
-  });
+  })
+  describe('when the cookie is present', () => {
+    beforeEach(() => {
+      (Cookies.get as jest.Mock).mockImplementation(() => JSON.stringify({
+        accessToken: '123456789'
+      }));
+      (api.default as jest.Mock).mockImplementation(() => ({
+        status: 401
+      }));
+    })
+    it('validates the token via the api', async () => {
+      const apiSpy = jest.spyOn(api, 'default');
+      await act(async () => {
+        await renderComponentWithRouting();
+      });
+      expect(apiSpy).toHaveBeenCalledWith(`${process.env.REACT_APP_API_URL}/google/authenticate`, {
+        token: '123456789',
+      }, {
+        method: 'POST'
+      })
+    })
+    describe('when the api identifies the token as invalid',  () => {
+      beforeEach(() => {
+        (api.default as jest.Mock).mockImplementation(() => ({
+          status: 401
+        }));
+      })
+      it('redirects the user to the login page', async () => {
+        await act(async () => {
+          const { renderedHistory, renderedLocation } = await renderComponentWithRouting();
+          testHistory = renderedHistory;
+          testLocation = renderedLocation;
+        });
+        if (!testLocation) {
+          return
+        }
+        expect((testLocation as Location).pathname).toBe("/login");
+      })
+    })
+    describe('when the api identifies the token as valid', () => {
+      beforeEach(() => {
+        (api.default as jest.Mock).mockImplementation(() => ({
+          status: 201
+        }));
+      })
+      it('does not redirect the user to the login page', async () => {
+        await act(async () => {
+          const { renderedHistory, renderedLocation } = await renderComponentWithRouting();
+          testHistory = renderedHistory;
+          testLocation = renderedLocation;
+        });
+        if (!testLocation) {
+          return
+        }
+        expect((testLocation as Location).pathname).not.toBe("/login");
+      })
+    })
+  })
 });
