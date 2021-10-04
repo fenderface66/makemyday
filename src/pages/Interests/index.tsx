ee70202538505c788e69f5825eafc30d896d7463
Robbie <r.heygate@gmail.com>
2021-10-04 16:33:14 +0100

    Added loading screen to interests page

--- src/pages/Interests/index.tsx
+++ src/pages/Interests/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ErrorMessage, Field, Form, Formik, FormikProps} from "formik";
 import styled from 'styled-components';
 import Masonry from 'react-masonry-component';
+import { css } from "@emotion/react";
 import images, {Image} from './images';
 import checkmark from "../../assets/checkmark.png"
+import ClipLoader from "react-spinners/ClipLoader";
 
 
 const SelectableImageContainer = styled.div`
   position: relative;
   cursor: pointer;
   max-width: 300px;
+  padding: 10px;
 `;
 
 const BackgroundImage = styled.img`
@@ -33,7 +36,7 @@ const Checkbox = styled(Field)`
 `
 
 const Overlay = styled.div`
-  width: 100%;
+  width: 300px;
   height: 65px;
   overflow: hidden;
   position: absolute;
@@ -42,6 +45,24 @@ const Overlay = styled.div`
   background-repeat: no-repeat;
   z-index: 9;
 `;
+
+const override = css`
+  display: block;
+  position: absolute;
+  top:0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+`;
+
+const LoadingScreen = styled.div`
+  background-color: white;
+  height: 100vh;
+  width: 100vw;
+  z-index: 10;
+  position: absolute;
+`
 
 const SelectableImage = ({ image, checked } : {image: Image, checked: boolean}) => (
   <SelectableImageContainer>
@@ -52,14 +73,22 @@ const SelectableImage = ({ image, checked } : {image: Image, checked: boolean}) 
   </SelectableImageContainer>
 );
 
+const MasonryContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
 type Values = {
   interests: string[]
 }
 
 const Interests = () => {
+  const [layoutComplete, setLayoutComplete] = useState<boolean>(false);
   return (
     <>
-      <h1>Interests</h1>
+      {!layoutComplete ? <LoadingScreen>
+        <ClipLoader loading={!layoutComplete} css={override} />
+      </LoadingScreen> : null}
       <Formik
         initialValues={{
           interests: [],
@@ -68,15 +97,20 @@ const Interests = () => {
       >
         {({ isSubmitting, values }: FormikProps<Values>) => (
           <Form>
-            <div role="group" aria-labelledby="checkbox-group">
-                <Masonry>
+            <MasonryContainer role="group" aria-labelledby="checkbox-group">
+                <Masonry
+                  options={{
+                    fitWidth: true
+                  }}
+                  onLayoutComplete={() => setLayoutComplete(true)}
+                >
                   {images.map(image => (
                     <label>
                       <SelectableImage image={image} checked={values.interests.includes(image.name) } />
                     </label>
                   ))}
                 </Masonry>
-            </div>
+            </MasonryContainer>
             <ErrorMessage name="requested_day_periods" />
             <button type="submit" disabled={isSubmitting}>
               Submit
